0f0764d68f1ed497794ad9b85e56c8ef933db498
Mstufing <mstufingms11@gmail.com>
2023-04-20 16:16:28 +0300

    added substr and replace methods

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
-const name = "Soft Club";
-console.log(name.substring(0, 4));
+// ? Substring Method
+
+// const name = "Soft Club";
+// console.log(name.substring(0, 4));
+
+
+// ? Substr() Method
+
+// let text = "I'm  learning JavaScript";
+// let result = text.substr(0, 14);
+// console.log(result);
+
+// let text1 = "I'm  learning JavaScript";
+// let result1 = text.substr(14, 10);
+// console.log(result1);
+
+// ? Replace() Method
+
+let text = "I'm learning C++";
+let result = text.replace("C++", "JavaScript");
+
+console.log(result);
